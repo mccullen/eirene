0229a6a91ca01b4a4ff917395aab37ed5daa064b
mccullen <mccullen_j@lynchburg.edu>
2024-07-21 23:44:52 -0400

    Update homepage so it includes reference to ohdsi sql

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+const linkClassName = "text-blue-500 hover:underline";
 
 export default function Home({db, test}: any) {
   return (
@@ -6,27 +7,39 @@ export default function Home({db, test}: any) {
       <header className="mb-8">
         <h1 className="text-2xl font-semibold mb-4">Welcome to Eirene</h1>
         <p className="text-lg text-gray-700">
-          This is a playground for working with the OMOP CDM, using the <Link
-            href="https://github.com/OHDSI/EunomiaDatasets"
-            className="text-blue-500 hover:underline"
+          This is a playground for working with the OMOP CDM, using the&nbsp;
+          <a 
+            href="https://github.com/OHDSI/EunomiaDatasets" 
+            className={linkClassName} 
+            target="_blank" 
+            rel="noopener noreferrer"
           >
             Eunomia Datasets
-          </Link>.
-          Please note that queries must be written in SQLite. For more information on SQLite, visit the <Link
-            href="https://www.sqlite.org/"
-            className="text-blue-500 hover:underline"
-          >
-            SQLite Documentation
-          </Link>.
+          </a>.
+          Please note that queries must be written in&nbsp;
+            <a 
+              className={linkClassName} 
+              href="https://www.sqlite.org/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              SQLite
+            </a> or&nbsp;
+            <a 
+              className={linkClassName} 
+              href="https://ohdsi.github.io/TheBookOfOhdsi/SqlAndR.html" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              OHDSI SQL
+            </a>. 
         </p>
       </header>
 
       <main>
         <p className="text-lg text-gray-700">
-          You can explore and run queries in SQLite in the <Link
-            href="/playground"
-            className="text-blue-500 hover:underline"
-          >
+          You can explore and run queries in the&nbsp; 
+          <Link href="/playground" className={linkClassName}>
             Playground
           </Link> section.
         </p>
