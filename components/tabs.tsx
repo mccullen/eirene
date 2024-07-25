4dc1c3e41f106bcb89c64f1f93279a35447a4646
mccullen <mccullen_j@lynchburg.edu>
2024-07-25 00:20:12 -0400

    Add comments

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -1,4 +1,7 @@
 export default function Tabs ({n, activeTab, onClick}){
+    // Make sticky at top so it doesn't move when you scroll down.
+    // Make it sticky at left so it doesn't move when you scroll horizontally
+    // Set z so it floats on top of the result tables, which is why you also need bg-white
     return (
         <ul className="sticky top-0 left-0 z-10 bg-white flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
             {
