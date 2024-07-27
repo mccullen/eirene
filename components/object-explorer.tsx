dac77e4bfdf68ded207e2c95680b556f14b4107d
mccullen <mccullen_j@lynchburg.edu>
2024-07-27 14:57:23 -0400

    Work on adding collapse to obj explorer

--- components/object-explorer.tsx
+++ components/object-explorer.tsx
@@ -1,12 +1,21 @@
 import { useContext } from "react";
 import { GlobalContext } from "./global-provider";
+import Collapsible from "./collapsible";
 
 export default function ObjectExplorer() {
     const { databases } = useContext(GlobalContext);
     return (
         <div id="obj-explorer">
             {Object.keys(databases).map(dbName => {
-                return ( <div>{dbName}</div>);
+                
+                const items = [
+                    { title: 'Section 1', content: <h1 className="bg-gray-500">Content for section 1</h1>, open: false },
+                    { title: 'Section 2', content: 'Content for section 2', open: false },
+                    { title: 'Section 3', content: 'Content for section 3', open: false },
+                  ];
+                return ( 
+                    <Collapsible items={items} />
+                );
             })}
         </div>
     );
