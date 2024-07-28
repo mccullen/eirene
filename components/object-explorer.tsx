6748a3331fc2cfd259a10dc764a155f9f3cc3a03
mccullen <mccullen_j@lynchburg.edu>
2024-07-27 23:45:22 -0400

    Add tables/cols to obj explorer

--- components/object-explorer.tsx
+++ components/object-explorer.tsx
@@ -7,12 +7,31 @@ export default function ObjectExplorer() {
     return (
         <div id="obj-explorer">
             {Object.keys(databases).map(dbName => {
+                let dbObj = databases[dbName];
+                let content = dbObj.tables.map((t, i) => {
+                    let tableContent = t.columns.map((c, i) => {
+                        return (
+                            <div key={`col-${i}`}>{c.name}</div>
+                        );
+                    });
+                    debugger;
 
+                    let tableItem = { title: t.name, content: <>{tableContent}</>, open: false };
+                    let tableItems = [tableItem];
+                    return (
+                        <Collapsible key={`table-${i}`} items={tableItems} />
+                    );
+                });
+                let item = { title: dbName, content: <>{content}</>, open: false}
+                let items = [item];
+
+/*
                 const items = [
                     { title: 'Section 1', content: <h1 className="bg-gray-500">Content for section 1</h1>, open: false },
                     { title: 'Section 2', content: 'Content for section 2', open: false },
                     { title: 'Section 3', content: 'Content for section 3', open: false },
                   ];
+                  */
                 return ( 
                     <Collapsible key={dbName} items={items} />
                 );
